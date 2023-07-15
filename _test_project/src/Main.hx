cb6b25b6e3151ab8ab3134a81e2b746459a9285b
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-07-14 22:59:02 -0400

    Add custom mod to test project

--- _test_project/src/Main.hx
+++ _test_project/src/Main.hx
@@ -1,11 +1,23 @@
 // Let the Haxe compiler know this module wants to use Evil Haxe mods.
-// The only mod this module is using is the "pipe" mod.
-#evil(pipe)
+// Let's use the "pipe" mod and our custom "my_mod".
+#evil(pipe, my_mod)
 
 package;
 
 function main() {
+	// Built-in "pipe" mod.
 	"test" |> repeat(3) |> trace;
+
+	// New "loop" feature created by "my_mod".
+	var i = 1;
+	loop {
+		i *= 2;
+		if(i > 20) {
+			break;
+		} else {
+			trace(i);
+		}
+	}
 }
 
 function repeat(number: Int, s: String): String {
--- _test_project/src/Mod.hx
+++ _test_project/src/Mod.hx
@@ -2,4 +2,33 @@ package;
 
 function init() {
 	trace("Called in conspire-time!");
+
+	// Register our mod.
+	Evil.addParserMod("my_mod", {
+		onExpr: parseExpr
+	});
+}
+
+function parseExpr(tokenStream: evil.macro.TokenStream): Null<haxe.macro.Expr> {
+	// Check the current token...
+	return switch(tokenStream.peek().token) {
+
+		// Check for "loop" token...
+		case Const(CIdent("loop")): {
+
+			// Move on from the current token to the next one.
+			tokenStream.consume();
+
+			// Parse the next expression.
+			final expr = tokenStream.nextExpr();
+
+			// Generate the expression our new syntax will generate.
+			macro while(true) $expr;
+		}
+
+		// Otherwise, return `null` to use default parsing behavior.
+		case _: {
+			null;
+		}
+	}
 }
