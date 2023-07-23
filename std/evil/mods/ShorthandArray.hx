0ecdc5f4cd0c2f75769adc33be6d7b4c384cbb90
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-07-23 19:02:37 -0400

    Added Shorthand Array Type mod

---
+++ std/evil/mods/ShorthandArray.hx
@@ -0,0 +1,42 @@
+package evil.mods;
+
+#if (macro || display)
+
+import haxe.macro.Expr;
+import evil.macro.TokenStream;
+
+/**
+	Initializes the Shorthand Array mod.
+**/
+function init() {
+	Evil.addParserMod("shorthand_array", {
+		onAfterType: onAfterType
+	});
+}
+
+/**
+	If `[]` proceeds a type, wrap it with `Array`.
+**/
+function onAfterType(stream: TokenStream, type: { type: ComplexType, pos: Position }): Null<{ type: ComplexType, pos: Position }> {
+	return switch(stream.peek().token) {
+		case BkOpen: {
+			stream.consume();
+			final t2 = stream.peek();
+			switch(t2.token) {
+				case BkClose: {
+					stream.consume();
+					final prevType = type.type;
+					{
+						type: macro : Array<$prevType>,
+						pos: stream.posUnion(type.pos, t2.pos)
+					};
+				}
+			}
+		}
+		case _: {
+			null;
+		}
+	}
+}
+
+#end
